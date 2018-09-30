8afca9b9ad1ae5ac449c70852f48a2d27573a49c
batzoo <40250093+batzoo@users.noreply.github.com>
2018-09-30 12:42:23 +0200

    ez

--- header.php
+++ header.php
@@ -1,11 +1,18 @@
 <header>
-	<title>LeBonFoin</title>
+	<div class="header">
+    <link rel="stylesheet" href="css/headercss.css" />
+	<title>
+	LeBonFoin</title>
+	<h1 class="headertitle">LeBonFoin</h1>
+	<nav>
 	<div class="Menu">
-		<ul>
+		<ul>	
 			<li>Products</li>
 			<li>Cart</li>
 			<li>Search</li>
 			<li>Log In</li>
 			<li>Sign Up</li>
 		</ul>
+	</div>
+</nav>
 </header>
