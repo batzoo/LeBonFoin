5f7a7c86eff8deda4f09c68b5c9d8dc33ec4e21c
batzoo <40250093+batzoo@users.noreply.github.com>
2018-09-30 12:10:40 +0200

    Header

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+	<title>LeBonFoin</title>
+	<div class="Menu">
+		<ul>
+			<li>Products</li>
+			<li>Cart</li>
+			<li>Search</li>
+			<li>Log In</li>
+			<li>Sign Up</li>
+		</ul>
+</header>
