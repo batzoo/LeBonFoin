9af399ca2043a115b082e09a5d21ba3c3e197c69
Germain.Paresys <43474403+MisteriusAC@users.noreply.github.com>
2018-10-05 15:13:38 +0200

    FinAccountPage

---
+++ CreateAccountPage.php
@@ -0,0 +1,25 @@
+<head>
+    <title>LeBonFoin</title>
+    <link rel="stylesheet" href="css/format.css" />
+    <link rel="stylesheet" href="css/common.css" />
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+    <?php include "header.php";?>    
+    
+    <section>
+       <div href="main.html">
+        	<img id="welcomeMainImage" src="Images/logo.jpg">
+        </div>
+    </section>
+
+    <div class=Formulaire>
+    <form action='answer-account.php' method='POST'>
+        <label for="Pseudo">Pseudo :</label><input type="text" name="pseudo" /><br />
+        <label for="mdp1">Mot de passe :</label><input type="text" name="mdp1" /><br />
+        <label for="mdp2">Confirmer mdp :</label><input type="text" name="mdp2" /><br />
+        <input type='submit' >
+    </form>
+    </div>
+    <?php include "footer.php";?>
+</body>
