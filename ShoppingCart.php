a7803e7b39cd941c082b3a756b8b2e4957fee4ad
mokimok8 <roubaudphilemon@gmail.com>
2018-10-05 14:44:28 +0200

    Converting shoppingcart in php

--- ShoppingCart.php
+++ ShoppingCart.php
@@ -5,30 +5,14 @@
 <head>
     <title>LeBonFoin.fr</title> 
     <link rel="stylesheet" href="css/Produits.css" />
+    <link rel="stylesheet" href="css/Header.css" />
 </head>
 
 <!--BODY SECTION-->
 <body>
 
     <!--HEADER SECTION-->
-	<header><a id="headertitle" href="main.html">LeBonFoin.fr</a>
-		<nav>
-			<div class="menuCategory">
-			    <a href="search.html">Search</a>
-			</div>
-			<div class="menuCategory">
-				
-		    	<span class="navtitle">Product</span>
-		    	<div class="withSeveralLinks">
-			    	<a href="foin.html">foin</a>
-			    	<a href="Tracteurs.html">Tracteurs</a>
-				</div>
-			</div>
-			<div class="menuCategory">
-			    <a href="contact.html">Contact us</a>
-			</div>
-		</nav>
-	</header>
+	<?php include "header.php";?>
     
 
     <!--MAIN CONTENT-->
