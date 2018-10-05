58e77f3f6e3e5590da8fe8f782255a553815fe82
mokimok8 <roubaudphilemon@gmail.com>
2018-10-05 15:39:23 +0200

    productpage skeletton

---
+++ ProductPage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<!--HEAD SECTION-->
+<head>
+    <title>LeBonFoin.fr</title> 
+    <link rel="stylesheet" href="css/ProductPage.css" />
+    <link rel="stylesheet" href="css/Header.css" />
+</head>
+
+<!--BODY SECTION-->
+<body>
+
+    <!--HEADER SECTION-->
+	<?php include "header.php";?>
+    
+
+    <!--MAIN CONTENT-->
+    <section>
+        <h1>PRODUCTNAME</h1>
+        <img id="productPage_main_image" src="Images/foin.jpg">
+
+
+    
+
+    </section>
+    <?php include "footer.php";?>
+</body>
+</html>
