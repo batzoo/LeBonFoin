713f7cc31ccd6128bbe1eebbc99c1cee136d5601
AlexisHemery <alexis.hemery@isen.yncrea.fr>
2018-10-05 14:39:21 +0200

    Header

---
+++ Produits.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LeBonFoin.fr</title> 
+    <link rel="stylesheet" href="css/Produits.css" />
+    <link rel="stylesheet" href="css/header.css" />
+
+</head>
+<body>  
+	<?php include "header.php";?>
+    <section>
+        	<img id="welcomeMainImage" src="Images/logo.jpg">
+        <div class=Qualification>
+        <h1>Foins de qualite superieur</h1>
+    	</div>
+        	<img id="FoinDeQualite" src="Images/FoinDeQualite.jpg"> 
+         <div class=Qualification>
+        <h1>Moisonneuse-batteuse John Deere</h1>
+    	</div>
+    		<img id="Moisonneuse" src="Images/Moisonneuse.jpg"> 
+    	<div class=Qualification>
+        <h1>Mangeoire a vaches grande capacite</h1>
+    	</div>
+    		<img id="Mangeoire" src="Images/Mangeoire.jpg"> 
+    </section>
+    <footer>
+        <div><a>Toutes nos equipes donnent le maximum pour vous satisfaire</a></div>
+    </footer>
+</body>
+</html>
