7679f1af7c0ffb00c9f62097e29912428a16118d
baptiste <bapt.caen@gmail.com>
2018-10-10 11:06:50 +0200

    Debut pageProduit

--- ProductPage.php
+++ ProductPage.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
+<?php
+// Sous WAMP (Windows)
+$bdd = new PDO('mysql:host=localhost;dbname=LeBonFoin;charset=utf8', 'root', '');
+
+?>
 
 <!--HEAD SECTION-->
 <head>
@@ -13,10 +18,14 @@
 
     <!--HEADER SECTION-->
 	<?php include "header.php";?>
-    
+    <?php $request = $bdd->query('SELECT name, description, unit_price FROM products');
 
-    <!--MAIN CONTENT-->
-    
+    while ($produit = $request->fetch(PDO::FETCH_ASSOC)) // Chaque entrée sera récupérée et placée dans un array.
+{
+  echo $produit['name'], '  description :  ', $produit['description'], '   !!! Coute : ', $produit['unit_price'], ' euros /////////////////// ';  
+
+}
+    ?>
 
 
 
