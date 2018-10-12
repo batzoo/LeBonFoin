e3178761ba40b3ad8da150a13000d89d4c283a26
baptiste <bapt.caen@gmail.com>
2018-10-12 13:35:37 +0200

    cc

--- Produits.php
+++ Produits.php
@@ -2,16 +2,18 @@
 <html>
 <head>
     <link rel="stylesheet" href="css/Produits.css" />
+
    <?php $bdd = new PDO('mysql:host=localhost;dbname=LeBonFoin;charset=utf8', 'root', '') ;?>
 </head>
  <?php include "header.php";?>
 <body>
+     <STYLE>A {text-decoration: none;} </STYLE>
     <section>
         <div id="conteneur">
         <div class="element">
             <a href="foin.php">
             <p><img src="Images/foin.jpg" width="200"> </p>
-            <p><?php $reponse=$bdd->query('SELECT name,unit_price FROM products WHERE id="1"');
+            <p id="texte"><?php $reponse=$bdd->query('SELECT name,unit_price FROM products WHERE id="1"');
                 $nom=$reponse->fetch();
             echo($nom['name']);?>
             <br/>
@@ -22,7 +24,7 @@
         	
          <div class="element">
             <p><img src="Images/Moisonneuse.jpg"width="200"></p>
-            <p><?php $reponse=$bdd->query('SELECT name,unit_price FROM products WHERE id="2"');
+            <p id="texte"><?php $reponse=$bdd->query('SELECT name,unit_price FROM products WHERE id="2"');
                 $nom=$reponse->fetch();
             echo($nom['name']);?>
             <br/>
@@ -34,7 +36,7 @@
     		 
     	<div class="element">
             <p><img  src="Images/Mangeoire.jpg" width="200"></p>
-            <p><?php $reponse=$bdd->query('SELECT name,unit_price FROM products WHERE id="3"');
+            <p id="texte"><?php $reponse=$bdd->query('SELECT name,unit_price FROM products WHERE id="3"');
                 $nom=$reponse->fetch();
             echo($nom['name']);?>
             <br/>
