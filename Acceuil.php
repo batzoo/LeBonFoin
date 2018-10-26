4dc63480d167f881e8708bfcc4caea3895a27334
Germain.Paresys <43474403+MisteriusAC@users.noreply.github.com>
2018-10-26 14:53:58 +0200

    panier-dynamique

--- Acceuil.php
+++ Acceuil.php
@@ -16,8 +16,8 @@ $pseudoCo=$_SESSION['pseudo'];
 ?>
 
 <div class='titre'>
-    <h1>'Bonjour <?php echo ($pseudoCo) ?>' </h1></div>
-    <h1>'Bienvenue sur LeBonFoin.fr'</h1></div>
+    <h1>Bonjour <?php echo ($pseudoCo) ?> </h1></div>
+    <h1>Bienvenue sur LeBonFoin.fr</h1></div>
 <div class='soustitre'>
     <h1>Fournisseur officiel des agriculteurs des Haut-de-France depuis 1892 !</h1>
     <h1>Bonne navigation ! :)</h1>
