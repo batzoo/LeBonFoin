cbd39bcbb9f6c6ece088a425886cd75e14dcbcd6
Germain.Paresys <43474403+MisteriusAC@users.noreply.github.com>
2018-11-23 15:59:11 +0100

    Valide2

--- actions/DeleteAndCreate.php
+++ actions/DeleteAndCreate.php
@@ -1,7 +1,6 @@
 
 <?php 
 
-	$id=$_SESSION['id'];
 
 	$reponse = $bdd->prepare('SELECT id FROM orders WHERE user_id=:id AND status="CART"');
 			$reponse->bindParam(':id',$id);
@@ -12,14 +11,13 @@
 			$reponse2->bindParam(':id1',$cartid);
 			$reponse2->bindParam(':id2',$id);
 			$reponse->execute();
-			echo 'billed';
 
 
-	$createur = $bdd->prepare('INSERT INTO orders(user_id,type,status,amount,billing_adress_id,delivery_adress_id) VALUES (:usrid,CART,CART,0,NULL,NULL)');
+
+	$createur = $bdd->prepare('INSERT INTO orders(user_id,type,status,amount,billing_adress_id,delivery_adress_id) VALUES (:usrid,"CART","CART",0,NULL,NULL)');
 	$createur -> execute(array(
             'usrid' => $id
           ));
-			echo 'new';
 
 
 ?>
