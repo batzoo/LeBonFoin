a23731c8f742d58ff7139c2164d1f699756c0f0f
mokimok8 <roubaudphilemon@gmail.com>
2018-11-27 18:43:14 +0100

    Update DeleteAndCreate.php

--- actions/DeleteAndCreate.php
+++ actions/DeleteAndCreate.php
@@ -1,20 +1,10 @@
 
 <?php 
 
-	$id=$_SESSION['id'];
-	var_dump($id);
-
-	$reponse = $bdd->prepare('SELECT id FROM orders WHERE user_id=:id AND status="CART"');
-			$reponse->bindParam(':id',$id);
-			$reponse->execute();
-			$cartid = $reponse->fetch();
-
-	var_dump($cartid[0]);
-	$test = "BILLED";
 	$reponse2 = $bdd->prepare('UPDATE orders SET status=:status WHERE id=:id1 AND user_id=:id2');
-			$reponse2->bindParam(':id1',$cartid[0]);
-			$reponse2->bindParam(':id2',$id);
-			$reponse2->bindParam(':status',$test);
+			$reponse2->bindParam(':id1',$_SESSION['idcart']);
+			$reponse2->bindParam(':id2',$_SESSION['id']);
+			$reponse2->bindParam(':status',"BILLED");
 
 			$reponse2->execute();
 
